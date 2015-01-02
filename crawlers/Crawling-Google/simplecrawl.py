c46b5d26fc613dda499ef207833dbacc0e35cc7e
im-rodrigo <rodrigopala91@gmail.com>
2015-01-02 05:19:24 -0800

    updating crawling code beginning

--- crawlers/Crawling-Google/simplecrawl.py
+++ crawlers/Crawling-Google/simplecrawl.py
@@ -1,7 +1,9 @@
-import urllib2
-from lxml import html
+import lxml
+
+
+# To address paging in Google
+PAGE = 0
 
 # url and query string from PART I
 # this is a custom range from Jan 1, 2000 to Jan 1, 2001
-url = 'https://www.google.com/search?q=new+york+times&tbs=cdr%3A1%2Ccd_min%3A1%2F1%2F2000%2Ccd_max%3A1%2F1%2F2001&start=10'
-
+URL = 'https://www.google.com/search?q=new+york+times&tbs=cdr%3A1%2Ccd_min%3A1%2F1%2F2000%2Ccd_max%3A1%2F1%2F2001&start=' + PAGE*10
