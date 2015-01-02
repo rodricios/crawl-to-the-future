b3302f85eee7799ec42e691c25b52f6961a912c3
im-rodrigo <rodrigopala91@gmail.com>
2015-01-02 05:32:08 -0800

    added str(..) to simplecrawl.py and readme

--- crawlers/Crawling-Google/simplecrawl.py
+++ crawlers/Crawling-Google/simplecrawl.py
@@ -7,7 +7,7 @@ PAGE = 0
 # this is a custom range from Jan 1, 2000 to Jan 1, 2001
 URL = 'https://www.google.com/search?q=new+york+times&\
       tbs=cdr%3A1%2Ccd_min%3A1%2F1%2F2000%2Ccd_max%3A1%2F1%2F2001\
-      &start=' + PAGE*10
+      &start=' + str(PAGE*10)
 
 google_results = html.parse(URL)
 
